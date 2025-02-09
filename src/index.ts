3bfc952b297d8ded5514c0e83ace9c8f7f497a07
Vadim Korolev <49880037+VadimOnix@users.noreply.github.com>
2025-02-09 15:22:20 +0300

    feat(logger): add pino as logger
    with pino-pretty as default formatter

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-export function sum(a: number, b: number) {
-  return a + b
-}
+import 'dotenv/config'
+
+import chokidar from 'chokidar'
+import path from 'path'
+
+import { logger } from './logger'
+
+const watchDir = path.join(process.cwd(), 'samples')
+const watcher = chokidar.watch(watchDir, {
+  persistent: true,
+  awaitWriteFinish: true,
+  // optimize watch
+  depth: 2,
+})
+
+logger.info(`CACHE_DIR: ${watchDir}`)
+watcher.on('add', (path) => console.log(`File "${path}" has been added`))
