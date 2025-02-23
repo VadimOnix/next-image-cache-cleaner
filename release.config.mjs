fe7d5cb66a37bfa6c1d80c0f43641ac8fecd9a14
Vadim Korolev <49880037+VadimOnix@users.noreply.github.com>
2025-02-23 22:47:03 +0300

    feat(npm): configure automatic publishing in npm

--- release.config.mjs
+++ release.config.mjs
@@ -8,14 +8,9 @@ const config = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/git',
+    '@semantic-release/npm',
     '@semantic-release/github',
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: true,
-      },
-    ],
+    '@semantic-release/git',
   ],
 }
 
