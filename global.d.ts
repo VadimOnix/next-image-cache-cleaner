142df60dd5fdfb77f0df10ab9b918aa142aa24ad
Vadim Korolev <korolev-vr@zvuk.com>
2025-07-18 22:32:01 +0300

    fix: fix global types

--- global.d.ts
+++ global.d.ts
@@ -3,9 +3,9 @@ declare global {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production' | 'test'
       /* Acceptable percentage of file directory fullness. In range from 0.001 to 0.999 */
-      NICC_FULLNESS_PERCENT: number
+      NICC_FULLNESS_PERCENT: string
       /* Maximum capacity of directory in kilobytes (100Mb by default) */
-      NICC_MAX_CAPACITY: number
+      NICC_MAX_CAPACITY: string
       /* Cron setup string. See more: https://github.com/node-cron/node-cron?tab=readme-ov-file#cron-syntax */
       NICC_CRON_CONFIG: string
       /* Path to image cache directory */
